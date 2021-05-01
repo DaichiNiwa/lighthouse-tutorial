1fe82a36ba31967b4867e121512c6bf995c4e403
DaichiNiwa <daichi.niwa.100@gmail.com>
2021-05-01 19:24:34 +0900

    webの認証に変更（未完成）

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth:web')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/hoge', function ( ) {
+    return 'hoge';
+});
