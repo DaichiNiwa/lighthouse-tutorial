fe5372fd8cab1b15d2df3be51c33e602c264e1cc
DaichiNiwa <daichi.niwa.100@gmail.com>
2021-05-16 14:02:37 +0900

    ページネーションつきのResolverを作成

---
+++ app/GraphQL/Queries/UserBetweenResolver.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+
+class UserBetweenResolver
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args): Builder
+    {
+        \Log::info($_);
+        \Log::info($args);
+        $minAge = $args['min_age'];
+        $maxAge = $args['max_age'];
+        return User::whereBetween('age', [$minAge, $maxAge])->orderBy('age');
+    }
+}
