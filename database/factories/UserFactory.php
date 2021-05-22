2b666b249c11e2bf6d90067cf9b3042b5897cd35
DaichiNiwa <daichi.niwa.100@gmail.com>
2021-05-22 20:49:31 +0900

    マイグレーション追加

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'age' => $this->faker->numberBetween(0, 100),
+            'blood_type' => $this->faker->numberBetween(0, 3),
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
