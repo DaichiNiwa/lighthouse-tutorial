e14e7b97840802812620729251cff13856c0d571
DaichiNiwa <daichi.niwa.100@gmail.com>
2021-05-23 12:46:16 +0900

    ラッキーアイテムの取得メソッドを移設'

--- app/GraphQL/Queries/UserResolver.php
+++ app/GraphQL/Queries/UserResolver.php
@@ -2,6 +2,7 @@
 
 namespace App\GraphQL\Queries;
 
+use App\Enums\Items;
 use App\Models\User;
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
@@ -11,5 +12,19 @@ class UserResolver
     public function maskedEmail(User $user): string
     {
         return '*****' . substr($user->email, 5);
+    }
+
+    public function getLukeyItemsRandomly(): array
+    {
+        $allItems = Items::asArray();
+        $lukeyItemsCountToday = mt_rand(1, count($allItems));
+        shuffle($allItems);
+
+        $lukeyItems = [];
+        for ($i = 1; $i <= $lukeyItemsCountToday; $i++) {
+            $lukeyItems[] = array_pop($allItems);
+        }
+
+        return $lukeyItems;
     }
 }
